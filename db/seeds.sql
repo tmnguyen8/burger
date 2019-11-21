26938d8b3f7ad612b78a507f29ffc0b1909f6cd5
Trai Nguyen <tnguyen@at-pac.com>
2019-11-21 15:50:20 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Stack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Stack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Guys', false);
